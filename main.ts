0c60d23e4a1bcd9dcde16a66601c237ff2458754
codefox-dev9876 <knoll-tag-estrogen@duck.com>
2026-02-20 22:11:01 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
 alert("TESTING TESTING 1234!!!");
-let msg = "why are you here";
-console.log(msg);
+
+const dev = {
+  name: "Codefox",
+  age: 13,
+  code: true,
+  message: "What are you doing here???",
+};
+console.log(dev.name);
+console.log(dev.age);
+console.log(dev.message);
